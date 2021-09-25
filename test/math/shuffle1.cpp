12410b1253f77472ab422dfb06cb9e7aaf3e1174
izlyforever <izlyforever@outlook.com>
2021-09-25 22:23:11 +0800

    add shuffle test

---
+++ test/math/shuffle1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include "../../cpplib/math/mixed.hpp"
+
+int main() {
+  //freopen("in", "r", stdin);
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+
+  std::vector<int> a{1, 2, 5, 6, 8, 17};
+  KnuthShuffle(a);
+  for (auto &x : a) std::cout << x << ' ';
+  std::cout << '\n';
+
+  auto b = uniformChoose(1e9, 10);
+  for (auto &x : b) std::cout << x << ' ';
+  std::cout << '\n'; 
+  
+  return 0;
+}
