b4dccf06229bdcf472eb90424c5da2ec7b9d09db
izlyforever <izlyforever@outlook.com>
2021-10-27 21:06:33 +0800

    refine timer

--- cpplib/timer.hpp
+++ cpplib/timer.hpp
@@ -2,11 +2,11 @@
 #include <bits/stdc++.h>
 
 class Timer {
-  std::chrono::steady_clock::time_point start_;
+  std::chrono::steady_clock::time_point start;
  public:
-  Timer() : start_(std::chrono::steady_clock::now()) {}
-  void show() {
-    auto elapsedTime = std::chrono::steady_clock::now() - start_;
-    std::cerr << "Time used: " << elapsedTime.count() / 1'000'000 << "ms\n";
+  Timer() : start(std::chrono::steady_clock::now()) {}
+  void show(std::string s = {}) {
+    auto elapsedTime = std::chrono::steady_clock::now() - start;
+    std::cerr << "Time used[" << s << "]: " << elapsedTime.count() / 1000000 << "ms\n";
   }
 };
