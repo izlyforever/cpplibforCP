31a2fde140daaf88467d48ef1d66a61b2a879e03
izlyforever <izlyforever@outlook.com>
2021-11-26 18:07:40 +0800

    refine timer

--- cpplib/timer.hpp
+++ cpplib/timer.hpp
@@ -1,12 +1,13 @@
 #pragma once
 #include <bits/stdc++.h>
 
-class Timer {
-  std::chrono::high_resolution_clock::time_point start;
+class Timer final {
+  std::chrono::high_resolution_clock::time_point start_;
+  std::string name_;
  public:
-  Timer() : start(std::chrono::high_resolution_clock::now()) {}
-  void show(std::string s = {}) {
-    auto elapsedTime = std::chrono::high_resolution_clock::now() - start;
-    std::cerr << "[Time used: " << s << "] " << elapsedTime.count() / 1'000'000.0 << "ms\n";
+  Timer(std::string name = {}) : start_(std::chrono::high_resolution_clock::now()), name_(name) {}
+  ~Timer() {
+    auto elapsedTime = std::chrono::high_resolution_clock::now() - start_;
+    std::cerr << "[Time used: " << name_ << "] " << elapsedTime.count() / 1'000'000.0 << "ms\n";
   }
 };
