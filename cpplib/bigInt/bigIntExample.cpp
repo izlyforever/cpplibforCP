8bbbba8c0c1846f28b2dab8f67431dbf558c0e94
izlyforever <izlyforever@outlook.com>
2022-06-17 01:52:09 +0800

    format

--- cpplib/bigInt/bigIntExample.cpp
+++ cpplib/bigInt/bigIntExample.cpp
@@ -2,13 +2,13 @@
 #include "bigInt.hpp"
 
 int main() {
-	// freopen("in", "r", stdin);
-	std::string a, b;
-	std::cin >> a >> b;
-	BigInt10 A(a), B(b);
-	std::cout << A + B << '\n'; // https://www.luogu.com.cn/problem/P1601
-	std::cout << A - B << '\n'; // // https://www.luogu.com.cn/problem/P2142
-	std::cout << A * B << '\n'; // https://www.luogu.com.cn/problem/P1303
-	std::cout << A / B << '\n'; // https://loj.ac/p/164
-	return 0;
+  // freopen("in", "r", stdin);
+  std::string a, b;
+  std::cin >> a >> b;
+  BigInt10 A(a), B(b);
+  std::cout << A + B << '\n'; // https://www.luogu.com.cn/problem/P1601
+  std::cout << A - B << '\n'; // // https://www.luogu.com.cn/problem/P2142
+  std::cout << A * B << '\n'; // https://www.luogu.com.cn/problem/P1303
+  std::cout << A / B << '\n'; // https://loj.ac/p/164
+  return 0;
 }
